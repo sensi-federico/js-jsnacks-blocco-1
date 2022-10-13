60c1e12a935ce88df9a093b48b81b03a2826519c
sensiz <dev.federicosensi@gmail.com>
2022-10-13 12:11:12 +0200

    while

--- 3_snack/script.js
+++ 3_snack/script.js
@@ -2,14 +2,31 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti. 
 
-let numbersArray = [];
+// let numbersArray = [];
 
-for (let i = 0; i < 10; i++) {
-    const numbers = prompt('inserisci un numero');
-    numbersArray.push(numbers); 
+// for (let i = 0; i < 10; i++) {
+//     const numbers = prompt('inserisci un numero');
+//     numbersArray.push(numbers); 
+    
+// }
+
+// const somma = Number(numbersArray[0]) + Number(numbersArray[1]) + Number(numbersArray[2]) + Number(numbersArray[3]) + Number(numbersArray[4]) + Number(numbersArray[5]) + Number(numbersArray[6]) + Number(numbersArray[7]) + Number(numbersArray[8]) + Number(numbersArray[9]);
+
+// console.log(numbersArray);
+// console.log(somma);
+
+
+// Snack 3 blocco 1 con While:
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+let i = 0;
+let sum = 0;
+let number;
+
+while (i < 10) {
+    number = Number(prompt('Inserisci un numero'));
+    sum += number;
+    i++
 }
 
-const somma = Number(numbersArray[0]) + Number(numbersArray[1]) + Number(numbersArray[2]) + Number(numbersArray[3]) + Number(numbersArray[4]) + Number(numbersArray[5]) + Number(numbersArray[6]) + Number(numbersArray[7]) + Number(numbersArray[8]) + Number(numbersArray[9]);
-
-console.log(numbersArray);
-console.log(somma);
+console.log(sum);
